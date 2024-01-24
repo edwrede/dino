3025558f8516bba13e62bc867208ef1ecd4eaeeb
edwrede <ed@empirestate.co.za>
2024-01-24 15:11:00 +0200

    Added config file and made server address configurable

--- dino.go
+++ dino.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"os"
+
 	"github.com/edwrede/dino/dinowebportal"
 )
 
+type configuration struct {
+	WebServer string `json:"webserver"`
+}
+
 func main() {
-	dinowebportal.RubWebPortal("localhost:8787")
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := new(configuration)
+	json.NewDecoder(file).Decode(config)
+	dinowebportal.RubWebPortal(config.WebServer)
 }
